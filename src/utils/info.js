73c92ca78e50ebbd33cc674441510cbd71eff0db
Sean Johnson <sejohnson@linkedin.com>
2018-08-30 06:18:15 -0400

    Better handling of parse errors

--- src/utils/info.js
+++ src/utils/info.js
@@ -1,3 +1,5 @@
+import ParseError from 'srt-validator/srtparser/parseerror';
+
 export function cleanInfo(el) {
   while (el.lastChild) {
     el.removeChild(el.lastChild);
@@ -12,11 +14,18 @@ export function displayInfo(el, { message } = {}) {
   el.appendChild(li);
 }
 
-export function displayError(el, { message, lineNumber, validator } = {}) {
+export function displayError(el, error) {
+  const { message, lineNumber, validator } = error;
+
+  let errorText;
+  if (error instanceof ParseError) {
+    errorText = `Parse Error: ${message} on line ${lineNumber}`;
+  } else {
+    errorText = `${validator}: ${message} on line ${lineNumber}`;
+  }
+
   const li = document.createElement('li');
-  const textNode = document.createTextNode(
-    `${message} at ${lineNumber} from ${validator}`
-  );
+  const textNode = document.createTextNode(errorText);
   li.appendChild(textNode);
   li.classList.add('caption-validator__info', 'caption-validator__info--error');
   el.appendChild(li);
